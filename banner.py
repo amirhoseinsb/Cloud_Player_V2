4f488ec22ef92a3e813a86487d00194854c2d001
amirhosein sohrabi <amirhoseinsohrabi.official@gmail.com>
2022-08-06 15:33:03 +0430

    add banner

---
+++ banner.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  1 17:13:30 2022
+
+@author: amir
+"""
+
+BANNER = """                
+
+  ____ _                 _ ____  _                       
+ / ___| | ___  _   _  __| |  _ \| | __ _ _   _  ___ _ __ 
+| |   | |/ _ \| | | |/ _` | |_) | |/ _` | | | |/ _ \ '__|
+| |___| | (_) | |_| | (_| |  __/| | (_| | |_| |  __/ |   
+ \____|_|\___/ \__,_|\__,_|_|   |_|\__,_|\__, |\___|_|   
+                                         |___/           
+                         
+Created By : AmirhoseinSohrabi
+Gmail : amirhoseinsohrabi.official@gmail.com 
+
+----------------------------------------------           
+X == Exit          
+"""
+
+
+
+SOUND = """
+P == Play music
+X == Exit
+
+Enter your choice please: """
+
+PLAYER = """
+ ____            _      ____  _
+| __ )  __ _ ___(_) ___|  _ \| | __ _ _   _  ___ _ __
+|  _ \ / _` / __| |/ __| |_) | |/ _` | | | |/ _ \ '__|
+| |_) | (_| \__ \ | (__|  __/| | (_| | |_| |  __/ |
+|____/ \__,_|___/_|\___|_|   |_|\__,_|\__, |\___|_|
+                                      |___/
+---------------------------------------------------------
+Created By : AmirhoseinSohrabi
+
+Gmail : amirhoseinsohrabi.official@gmail.com
+---------------------------------------------------------
+
+X = Exit
+"""
+
+CHOICE_PLAY = """
+
+N == NEXT   B == PREVIOUS   S == STOP   ((P == PLAY))   X == EXIT
+
+C == CHANGE FILE
+
+Enter : """
+
+CHOICE_STOP = """
+
+N == NEXT   B == PREVIOUS   ((S == STOP))   P == PLAY   X == EXIT
+
+C == CHANGE FILE
+
+Enter : """
+
+PLAY = '''
+M == Manual Play
+A == Auto Play
+                  
+Please Enter the playback mode :'''
